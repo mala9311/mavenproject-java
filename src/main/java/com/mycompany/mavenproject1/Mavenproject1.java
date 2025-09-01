1cf2c0381d40480f0a79ec9d2a5fe5608084c44f
mala9311 <malakumari9311@gmail.com>
2025-09-01 19:29:28 +0530

    initial commit: java project

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+//     int   i = 1;
+//     int sum =0;
+//        while(i<=10){
+//            //if(i%2 != 0){
+//                sum += i;
+//               System.out.println(i);
+//            
+//            i++;
+//              //  System.out.println(sum);       
+//        }
+//        System.out.println("sum of the integer number" +sum);
+    for(int i=1;i<=4;i++){
+        for(int j=1; j<=i;j++){
+            System.out.print("*");
+    }
+        System.out.println();
+    }
+    // System.out.println();
+    }
+}
